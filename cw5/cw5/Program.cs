37eb940b4cff66eb67c7e02aaf26f4bc9b01de6a
fatema-mohammad-alenezi-472 <100342867+iamawesomee@users.noreply.github.com>
2022-03-05 11:47:09 +0300

    done

---
+++ cw5/cw5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace cw5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string CharacterName = "Steve";
+            int age = 90;
+            string Superpower1 = " I can fly through walls";
+            string Superpower2 = " I can go invisible";
+            int speed = 100;
+            Console.WriteLine("My name is {0}, my superpowers are {1}, {2}. I am {3} years old. I have a speed of {4} km/hr.", CharacterName, Superpower1, Superpower2, age, speed);
+
+        }
+    }
+}
